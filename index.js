40312fede3af9e993b12354bf3c281524eb1492a
mayank arya <mayanksunny786@gmail.com>
2026-05-30 18:32:47 +0530

    broadcasting-project-added

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const button=document.querySelector('.navbar-toggler')
-console.log(button)
-button.addEventListener('click',()=>{
-const navbar=document.querySelector('#navbar')
-navbar.classList.toggle('show')
-})
-
-const main=document.querySelector('.container-main')
-
- var a=main.clientWidth
-
-console.log(a)
-
+const button = document.querySelector(".navbar-toggler");
+console.log(button);
+button.addEventListener("click", () => {
+  const navbar = document.querySelector("#navbar");
+  navbar.classList.toggle("show");
+});
+const main = document.querySelector(".container-main");
+var a = main.clientWidth;
